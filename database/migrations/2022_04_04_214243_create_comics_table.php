a57012f36042433f9783d8f4c5a6ce0d7ea6fa04
iram96 <mariano.maselli@virgilio.it>
2022-04-05 01:45:08 +0200

    comic model + migr+ seeder + databaseSeeder

---
+++ database/migrations/2022_04_04_214243_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80)->unique();
+            $table->text('description');
+            $table->string('thumb')->nullable();
+            $table->float('price', 8, 2);
+            $table->string('series', 30)->unique();
+            $table->string('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
