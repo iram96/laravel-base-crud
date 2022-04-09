bd8fb4463fc23597dff1448918bdcb5c9d8200cc
iram96 <mariano.maselli@virgilio.it>
2022-04-09 16:25:01 +0200

    imported FontAwsome

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}" defer ></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @yield('script')
     <title>COMIC~STORE</title>
 </head>
@@ -17,7 +18,7 @@
             </div>
             <div class="container-fluid">
               <a class="navbar-brand" href="{{ route('comics.index') }}">
-               Comcis
+               Comics
             </a>
             </div>
           </nav>
